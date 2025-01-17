199894d4adce6c9698914e93bfab27eebc97825d
8ad40n <joy184110@gmail.com>
2025-01-17 18:30:38 +0600

    Initial server setup

---
+++ server/src/app.ts
@@ -0,0 +1,11 @@
+import cors from 'cors';
+import express, { Application } from 'express';
+
+const app: Application = express();
+
+// parsers
+app.use(express.json());
+app.use(cors());
+
+
+export default app;
